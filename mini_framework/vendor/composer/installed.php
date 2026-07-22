f1e3edf4253c3b0e6e86b10bd0b9e64b84941d2a
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-07-22 18:09:20 -0300

    Atualização do mini framework MVC

---
+++ mini_framework/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'leonardo/mini-framework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '36394bd567d93fb362f9b9b2372b88cd3c7083ec',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'leonardo/mini-framework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '36394bd567d93fb362f9b9b2372b88cd3c7083ec',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
