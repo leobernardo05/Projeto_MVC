ba1386f529557a5638e438af2ae78a56f456c4a7
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-07-23 10:25:48 -0300

    update

---
+++ mini_framework/App/Controllers/indexController.php
@@ -0,0 +1,13 @@
+<?php
+    namespace App\Controllers\
+
+    class indexController{
+        public function index(){
+            echo 'Chegamos ao indexController e disparamos a action index';
+        }
+
+        public function sobreNos(){
+            echo 'Chegamos ao IndexController e disparamamos a action sobreNos'
+        }
+    }
+?>
--- mini_framework/Public/index.php
+++ mini_framework/Public/index.php
@@ -1,5 +1,10 @@
 <?php
 
     require_once "../vendor/autoload.php";
+    $route = new \App\Route;
     echo 'funcionando';
+    echo '<hr>';
+    print_r($route->getUrl());
+    echo '<hr>';
+    print_r($route->getRoutes());
 ?>
